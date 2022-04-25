98ec0682a139349b473c4aba5bd6fd85dca5b3c4
liuyixin-louis <zxcxzcz@qq.com>
2022-04-25 11:28:05 +0800

    update

--- dl/train.py
+++ dl/train.py
@@ -49,3 +49,4 @@ class EarlyStopping:
             self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), self.path)
         self.val_loss_min = val_loss
+
